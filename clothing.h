3fa7e699c26b52607997725b8cd45a1402413f2e
clwangUSC <clwang@usc.edu>
2023-02-03 11:14:00 -0800

    upload to codio

---
+++ clothing.h
@@ -0,0 +1,25 @@
+#ifndef CLOTHING_H
+#define CLOTHING_H
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include "product.h"
+
+class Clothing : public Product {
+public:
+    Clothing(const std::string category, const std::string name, double price, int qty, const std::string size, std::string brand);
+    ~Clothing();
+
+    std::set<std::string> keywords() const;
+    std::string displayString();
+    void dump(std::ostream& os);
+private:
+    std::string size_;
+    std::string brand_;
+    std::set<std::string> keywords_;
+
+};
+
+#endif
---
+++ movie.h
@@ -0,0 +1,24 @@
+#ifndef MOVIE_H
+#define MOVIE_H
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include "product.h"
+
+class Movie : public Product {
+public:
+    Movie(const std::string category, const std::string name, double price, int qty, const std::string genre, std::string rating);
+
+    std::set<std::string> keywords() const;
+    std::string displayString();
+    void dump(std::ostream& os);
+private:
+    std::string genre_;
+    std::string rating_;
+    std::set<std::string> keywords_;
+
+};
+
+#endif
